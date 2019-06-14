b3f1bdd2e1745bdb781f8d9516cb9184472da936
shiva <shiva@192.168.1.3>
2019-06-14 15:54:45 +0530

    added tree structure(Binary tree) part theory

---
+++ DS/src/tree/TreeImplementation.java
@@ -0,0 +1,16 @@
+package tree;
+
+public class TreeImplementation {
+
+	//Height of tree=Height of root node
+	//Depth of root node=0
+	
+/*	Binary tree A-->B
+				 -->C
+				 
+				 Three types of binary tree--
+				 
+				 Strict binary tree
+				 Full binary tree (either 2 or zero child node)
+				 Complete binary tree*/
+}
